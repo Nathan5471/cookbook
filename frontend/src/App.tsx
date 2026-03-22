38582feabd2ea159dbaf24ac725c4e8cfed25ddd
Nathan Cloutier <nathanc4571@gmail.com>
2026-03-22 11:02:40 -0400

    Add colors/fonts (these will change)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <div className="w-full h-full flex flex-col bg-gray-100">
-      <div className="w-full h-screen bg-gray-300 flex flex-col items-center justify-center">
+    <div className="w-full h-full flex flex-col font-[Phantom_Sans]">
+      <div className="w-full h-[calc(100vh-144px)] bg-darker-background flex flex-col items-center justify-center">
         <h1 className="text-9xl font-bold text-gray-700">Cookbook</h1>
         <p className="text-3xl font-gray-500 mt-3 w-1/2">Make a food or cooking related project and get a custom Hack Club cookbook!</p>
       </div>  
-      <div className="w-full h-72 bg-gray-200 flex justify-center">
+      <div className="w-full h-72 bg-background flex justify-center">
         <div className="w-2/3 flex flex-col">
           <h2 className="text-4xl font-bold text-gray-600 mt-10">How it works?</h2>
           <div className="ml-2 flex flex-col">
@@ -21,7 +21,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="w-full h-64 bg-gray-200 flex justify-center">
+      <div className="w-full h-64 bg-background flex justify-center">
         <div className="w-2/3 flex flex-col">
           <h2 className="text-4xl font-bold text-gray-600 mt-10">Why a cookbook?</h2>
           <p className="text-2xl font-gray-500 mt-2">
@@ -29,7 +29,7 @@ function App() {
           </p>
         </div>
       </div>
-      <div className="w-full h-164 bg-gray-200 flex justify-center">
+      <div className="w-full h-128 bg-background flex justify-center">
         <div className="w-2/3 flex flex-col">
           <h2 className="text-4xl font-bold text-gray-600 mt-10">FAQ</h2>
           <div className="ml-2 flex flex-col">
